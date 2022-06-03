4039f3f219de5a49dea68374e2c995797b926cda
Laura Duque <duquelaura295@gmail.com>
2022-06-03 15:59:08 -0500

    update

--- Middleware/errorHandlers.js
+++ Middleware/errorHandlers.js
@@ -15,7 +15,7 @@ function boomError(err, req, res, next) {
   }
 }
 
-function errorHandler(err, req, res, next) {
+function errorHandler(err, req, res) {
   res.status(500).json({
     message: err.message,
     stack: err.stack,
